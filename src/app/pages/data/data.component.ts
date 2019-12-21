2165beb8114496ba43b4be25854b9e41135782e3
viju vijayan <vijuvjn@gmail.com>
2019-12-21 16:47:47 +0530

    up to 21.12.19 4.47pm

--- src/app/pages/data/data.component.ts
+++ src/app/pages/data/data.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
 export class DataComponent implements OnInit {
+
+  choice: string;
+  options: string[] = ['Office', 'PF Number'];
+
+
 
   constructor() { }
 
--- src/app/pages/data/data.module.ts
+++ src/app/pages/data/data.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DataRoutingModule } from './data-routing.module';
 import { DataComponent } from './data.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [DataComponent],
   imports: [
     CommonModule,
-    DataRoutingModule
+    DataRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatRadioModule,
+    MatFormFieldModule
   ]
 })
 export class DataModule { }
